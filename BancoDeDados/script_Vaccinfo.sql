c835fd4aacbf599388e76f6a013bd6407635aa1b
Gustavo Alcantara Ribeiro <125397719+ribgu@users.noreply.github.com>
2023-04-18 13:21:35 -0300

    atualizando script para a sprint2
    tabelas atualizadas segundo a modelagem de dados
    script de inserção de dados

--- BancoDeDados/script_Vaccinfo.sql
+++ BancoDeDados/script_Vaccinfo.sql
@@ -1,43 +1,66 @@
 CREATE DATABASE Vaccinfo; -- Criar banco de dados Vaccinfo
 USE Vaccinfo; -- Usar banco de dados Vaccinfo
 
--- Criar tabela usuario para armazenar dados dos usuarios registrados pelo site
--- A tabela deve conter uma chave primária, email, nome da empresa e senha.
--- Os campos citados acima são not null, ou seja, devem ser inseridas alguma informação para ser válido
+-- Criando a tabela empresa para armazenar dados da empresa contratante.
 -- O campo EMAIL tem uma constraint para verificar se o valor inserido é um email, ou seja, se possui um '@'
+
 CREATE TABLE empresa (
 	idEmpresa INT primary key auto_increment,
     nome VARCHAR(45),
     CNPJ CHAR(14),
     email VARCHAR(45),
-		constraint chk_email check (email like'%@%'));
-        
+		constraint chk_email check (email like'%@%'),
+	telefone VARCHAR(12));
+
+-- Criar tabela usuario para armazenar dados dos usuarios registrados pelo site
+-- O campo EMAIL tem uma constraint para verificar se o valor inserido é um email, ou seja, se possui um '@'
+-- A tabela usuario tem um campo com uma chave estrangeira para armazenar de qual empresa aquele usuário pertence
+
 CREATE TABLE usuario (
 	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
 	NomeCompleto VARCHAR(45) not null,
     email VARCHAR(45),
 		constraint chk_email check (email like'%@%'),
     telefone CHAR(14),
+    tipoDocumento VARCHAR(10),
+		constraint chk_tipo_documento check (tipoDocumento in('cpf', 'passaporte')),
     documento VARCHAR(45),
     dataNascimento DATE,
     fkEmpresa INT,
 		constraint usuarioEmpresa foreign key (fkEmpresa) references empresa (idEmpresa)
 );
+
+-- tabela login para armazenar os dados inserindos na tela de login e validar se o usuario é administrador
+
 CREATE TABLE login (
 	idLoginAdm INT PRIMARY KEY AUTO_INCREMENT,
     login VARCHAR(45),
     senha VARCHAR(45),
-    adiministrador TINYINT);
-    
-create table localizacao(
-idLocal int auto_increment,
-nome varchar(45),
-descricao varchar(45),
-fkEmpresa int,
-	constraint fkLocalEmpresa foreign key (fkEmpresa) references empresa(idEmpresa),
-    constraint pkLocal primary key (idLocal, fkEmpresa),
-fkEndereco int,
-	constraint fkLocalEndereco foreign key (fkEndereco) references endereco(idEndereco)
+    adiministrador TINYINT,
+    fkUsuario INT,
+		constraint loginUsuario foreign key (fkUsuario) references usuario (idUsuario));
+
+-- Criar tabela endereço para armazenar os dados da localização da empresa
+
+CREATE TABLE endereco (
+	idEndereco INT PRIMARY KEY AUTO_INCREMENT,
+    logradouro VARCHAR(45),
+    complemento VARCHAR(45),
+    bairro VARCHAR(45),
+    cidade VARCHAR(45),
+    estado CHAR(2)
+    );
+        
+-- Criar tabela local para armazenar os dados da localização a ser monitorada dentro do endereço da empresa
+
+create table localSensor(
+	idLocal int auto_increment,
+	nome varchar(45),
+	fkEmpresa int,
+		constraint fkLocalEmpresa foreign key (fkEmpresa) references empresa(idEmpresa),
+		constraint pkLocal primary key (idLocal, fkEmpresa),
+	fkEndereco int,
+		constraint fkLocalEndereco foreign key (fkEndereco) references endereco(idEndereco)
 );
     
     
@@ -45,56 +68,76 @@ fkEndereco int,
 -- Criação da tabela SENSOR para registrar os sensores usados no nosso projeto
 -- A tabela deve conter uma chave primária, o nome do sensor utilizado e o tipo de instalação (geladeira ou caminhão)
 -- O campo INSTALACAO tem uma constraint para verificar se o valor inserido é um 'geladeira' ou 'caminhão'
-CREATE TABLE SENSOR (
-	IDSensor int PRIMARY KEY AUTO_INCREMENT,
-   	NomeSensor VARCHAR(10),
-	Instalacao VARCHAR(15),
-	CONSTRAINT chk_instalacao CHECK (Instalacao IN('geladeira','caminhao'))
+
+CREATE TABLE sensor (
+	idSensor int PRIMARY KEY AUTO_INCREMENT,
+   	nomeSensor VARCHAR(45),
+	tipoInstalacao VARCHAR(45),
+		constraint chk_instalacao CHECK (tipoInstalacao IN('geladeira','caminhao')),
+	fkLocalSensor INT,
+		constraint local_sensor foreign key (fkLocalSensor) references localSensor(idLocal)
 );
+
 -- Criação da tabela LOTE para registrar os lotes de vacina registrados no nosso site
 -- A tabela deve conter uma chave primária, o código do lote registrado e uma variavel INT para ser foreign key
 -- O campo IDSensor(int) é uma foreign key que referencia o IDSensor na tabela SENSOR
-CREATE TABLE LOTE (
-	LoteReg int primary key auto_increment,
-	Lote varchar(100),
-	IDSensor int,
-	constraint fk_Lsensor foreign key (IDSensor) references SENSOR(IDSensor)
+
+CREATE TABLE lote (
+	idLote int primary key auto_increment,
+	descricao varchar(45),
+	fkSensor int,
+	constraint lotesensor foreign key (fkSensor) references sensor(idSensor)
    );
    
--- Criação da tabela TEMP para registrar a temperatura atual registrada no sensor
--- A tabela deve conter uma chave primária, um campo para o registro da data em que a temperatura foi inserida
--- No campo DtAtual, este, é configurado como 'datetime default current-timestamp'
--- A tabela também deve conter duas variaveis INT, que serão configuradas como foreign key
--- O campo IDSensor(int) e o Empresa(int), ambas são foreign key, na qual o primeiro se refere ao IDsensor da tabela Sensor
--- E o segundo, se refere ao UserID da tabela cadastro
-CREATE TABLE TEMP (
-	IDRegistro INT PRIMARY KEY AUTO_INCREMENT,
-	DtAtual DATETIME DEFAULT CURRENT_TIMESTAMP,
-	Temperatura VARCHAR(5),
-	Empresa int,
-	IDSensor int,
-	CONSTRAINT fk_sensor FOREIGN KEY (IDSensor) REFERENCES SENSOR(IDSensor),
-	CONSTRAINT fk_cadastro FOREIGN KEY(Empresa) REFERENCES CADASTRO(UserID)
+-- Criação da tabela registro para registrar a temperatura atual registrada no sensor
+-- No campo DtAtual, este, é configurado como 'datetime default current-timestamp' para capturar o a data e horario exato da captação
+CREATE TABLE registro (
+	idRegistro INT PRIMARY KEY AUTO_INCREMENT,
+	dataHoraRegistro DATETIME DEFAULT CURRENT_TIMESTAMP,
+	temperatura FLOAT,
+	fkSensor int,
+		CONSTRAINT registroSensor FOREIGN KEY (fkSensor) REFERENCES sensor(idSensor)
 );
--- Inserir um cadastro na tabela CADASTRO
-insert into cadastro values
-	(null, 'thiagogarcia526@gmail.com', 'senha123', 'Vaccinfo'),
-	(null, 'vitor.mramos@sptech.school', 'senha123', 'Pfizer');
+
+-- Inserir dados da empresa na tabela
+	INSERT INTO empresa VALUES
+		(null,'pfizer','46070868003699','contato@pfizer.com','+5511977424570'),
+		(null,'janssen','51780468000187','contato@janssen.com','+5511939479330');
+    
+-- inserir dados dos usuarios
+	INSERT INTO usuario VALUES
+		(null,'Gustavo ribeiro','gustavo.ribeiro@sptech.school','+5511963122168','cpf','53183297809','2005-04-30'),
+		(null,'Vitor Maciel','vitor.mramos@sptech.school','+5511992951528','cpf','53183297809','2000-08-16');
+    
+-- Inserir dados do usuario na tabela login
+	insert into login values
+		(null, 'gustavo.ribeiro@sptech.school', '#VaccInfo', '1','1'),
+		(null, 'vitor.mramos@sptech.school', '#VaccInfo', '0','2');
+	
+-- Inserir dados do endereço da empresa
+	insert into endereco values
+		(null, 'rua haddock lobo', 'travessa da avenida paulista', 'Jardins', 'São Paulo', 'SP'),
+		(null, 'rua cabral', 'travessa da avenida dom pedro', 'Pindorama', 'Rio de Janeiro', 'RJ');
+    
+-- Inserir dados do local aonde o sensor está
+	insert into localSensor values
+		(null,'Geladeira001','1','1'),
+		(null,'Caminhão001','2','2');
 
 -- Inserir o sensor LM35 tanto na geladeira quanto no Caminhão
-insert into sensor values
-	(null, 'LM35', 'Geladeira'),
-	(null, 'LM35', 'Caminhao');
+	insert into sensor values
+		(null, 'LM35', 'geladeira','1','1'),
+		(null, 'LM35', 'caminhao','2','2');
     
 -- Inserir dados sobre um lote
-insert into lote values
-	(null, 'BK1', 1),
-	(null, 'BK2', 2);
+	insert into lote values
+		(null, 'VI1', 1),
+		(null, 'VI2', 2);
     
 -- Inserir dados sobre o registro de temperatuda do sensor
-insert into temp (Temperatura, Empresa, IDSensor) values
-	('30', 1, 1),
-	('26', 2, 2);
+	insert into registro values
+		(null, null, '6','1'),
+		(null, null, '7','2');
 
 -- Caso hipotético, no qual o lote que estava no caminhão, vá para a geladeira, necessário um update
 update sensor set instalacao = 'Geladeira' where id = 2;
